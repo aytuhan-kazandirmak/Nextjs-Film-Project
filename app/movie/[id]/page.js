a2678f52509bb815cf526638f1a6c68d342f22b8
Aytuhan Kazandirmak <aytuhann@gmail.com>
2024-02-23 16:55:46 +0300

    created movie details and movie details loading page

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,11 +1,15 @@
 import MovieContainer from "@/containers/movie";
 import React from "react";
 import Movies from "../../../mocks/movies.json";
+import { notFound } from "next/navigation";
 
 const MoviePage = ({ params }) => {
   const movieDetail = Movies.results.find(
     (movie) => movie.id.toString() === params.id
   );
+  if (!movieDetail) {
+    notFound();
+  }
   console.log(params);
   return <MovieContainer movie={movieDetail} />;
 };
