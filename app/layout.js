ae2e9bb00ef40c6e91925c80bc80a0ee129224fe
Aytuhan Kazandirmak <aytuhann@gmail.com>
2024-02-23 14:02:32 +0300

    created reset css and global css

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,11 @@
-import "./globals.css";
-
+import "./styles/reset.css";
+import "./styles/global.css";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className="container">{children}</body>
     </html>
   );
 }
