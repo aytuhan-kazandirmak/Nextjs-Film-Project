14393935f681084324f7471d8ac87b7cf7662ba8
Aytuhan Kazandirmak <aytuhann@gmail.com>
2024-02-23 14:17:32 +0300

    created header component

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,15 @@
 import "./styles/reset.css";
 import "./styles/global.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/header";
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
-      <body className="container">{children}</body>
+      <body className="container">
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
